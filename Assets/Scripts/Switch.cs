e8ac6871d13226c1c3ad84ea77ca05c72c2d4a92
andrew93562 <andrew93562@gmail.com>
2023-12-11 13:19:23 -0800

    Added a 0 group switch
    0 group switches are activated on load

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -7,6 +7,14 @@ public class Switch : MonoBehaviour
     [SerializeField] GameEvent SwitchActivated;
     [SerializeField] int SwitchID;
 
+    private void Start()
+    {
+        if (SwitchID == 0)
+        {
+            SwitchActivated.Raise(this, SwitchID);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("collided with object");
